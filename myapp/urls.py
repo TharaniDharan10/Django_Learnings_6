69702a7de003596a82416d4a14043357cee0a617
TharaniDharan10 <dharunjisnu109@gmail.com>
2025-10-17 10:51:23 +0530

    inbuilt form

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('home/', views.home)
+    path('home/', views.home),
+    path('register/',views.register)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render, HttpResponse
+# inbuilt form   
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
 def home(req):
     return HttpResponse("Hello Home")
+
+def register(req):
+    form=UserCreationForm
+    if req.method=="POST":
+        regform=UserCreationForm(req.POST)
+        if regform.is_valid():
+            regform.save()
+            # we can see data stored in admin panel under home->user. 
+    return render(req,'register.html',{'form':form})
